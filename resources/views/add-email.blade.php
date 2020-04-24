fd01238b0f676051db18e87157f175af764da69f
Stephen Jude <StephenJudesuccess@gmail.com>
2020-04-24 01:34:42 +0100

    red add mail page

--- resources/views/add-email.blade.php
+++ resources/views/add-email.blade.php
@@ -8,30 +8,30 @@
     <img src="{{ asset('images/wave.svg') }}" class="absolute top-0 left-2/5">
     <div class="container mx-auto relative">
         <div class="flex flex-col md:flex-row items-center pt-32 pb-16 md:pb-0">
-            <div class="w-full shadow-lg  md:w-1/2 lg:w-1/2 mb-4 sm:mb-16 md:mb-0 bg-white rounded-lg">
-                <h3 class="pt-4 text-2xl text-center">Send Bulk Email</h3>
+            <div class="w-full shadow-lg md:w-1/2 lg:w-1/2 mb-4 sm:mb-16 md:mb-0 bg-white rounded-lg">
+                <h3 class="pt-4 text-2xl text-center">Add New Email</h3>
                 <form class="px-8 pt-6 pb-8 mb-4">
                     <div class="mb-4">
                         <label class="block mb-2 text-sm font-bold text-gray-700">
-                            Subject
+                            Name
                         </label>
                         <input
                             class="w-full px-3 py-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                            type="text" placeholder="Enter email subject" />
+                            type="text" placeholder="Enter your name" />
                     </div>
-                    <div class="mb-4">
+                    <div class="mb-8">
                         <label class="block mb-2 text-sm font-bold text-gray-700">
-                            Message
+                            Email
                         </label>
-                        <textarea
-                            class="w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
-                            rows="12" placeholder="Message goes here..."></textarea>
+                        <input
+                            class="w-full px-3 py-3 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
+                            type="email" placeholder="Enter your email" />
                     </div>
                     <div class="mb-6 text-center">
                         <button
                             class="w-full px-4 py-2 font-bold text-white bg-teal-400 rounded-full hover:bg-teal-500 focus:outline-none focus:shadow-outline"
                             type="button">
-                            Send
+                            Submit
                         </button>
                     </div>
                 </form>
